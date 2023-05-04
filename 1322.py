e8b6bcaea68d5276688a255d8fb6f00fe837056f
Mikhail Belyankin <undermind@users.noreply.github.com>
2023-05-04 16:06:11 +0300

    Create 1322.py

---
+++ 1322.py
@@ -0,0 +1,19 @@
+import sys
+#print (tokens)
+
+
+tokens = sys.stdin.read().split()
+#tokens = ['3', 'rdarcaaaabb']
+n=int(tokens[0])-1
+word=tokens[1]
+inv={}
+for i in range(len(word)):
+    inv[i]=word[i]
+print(inv)
+inv = sorted(inv.items(), key=lambda x:x[1])
+print(inv)
+for i in range(len(word)):
+    n=inv[n][0]
+    print(word[n],end='')
+print()
+
