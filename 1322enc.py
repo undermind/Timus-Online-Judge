a0088c4d170ba0d4d665392c458d9009eca121e2
Mikhail Belyankin <undermind@users.noreply.github.com>
2023-05-05 09:58:34 +0300

    fix generator for correct samples

--- 1322enc.py
+++ 1322enc.py
@@ -5,6 +5,7 @@ order="ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz"
 S="abracadabra"
 S="a"
 S="aaaaaaaaaaa"
+S="Exactpro"
 N=[S]
 for i in range(1,len(S)):
     S=S[1:]+S[0]
@@ -13,10 +14,11 @@ for i in range(1,len(S)):
 S=S[1:]+S[0]
 N.sort()
 S1=""
+answ=-1
 for i in range(0,len(S)):
-    if N[i]==S:
-        print(i+1)
+    if (N[i]==S)and(answ<0):
+        answ=i+1
     S1+=N[i][-1]
 
-
+print(answ)
 print(S1)
