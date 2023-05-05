927b841d812db28d68006fbe6ebe84485c7d31c5
Mikhail Belyankin <undermind@users.noreply.github.com>
2023-05-05 09:01:34 +0400

    1322 rewritten but same f*** time limint in test 9

--- 1322.py
+++ 1322.py
@@ -1,16 +1,18 @@
 import sys
 #print (tokens)
+order="ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz"
 
-
-tokens = sys.stdin.read().split()
-#tokens = ['3', 'rdarcaaaabb']
+#tokens = sys.stdin.read().split()
+tokens = ['3', 'rdarcaaaabb']
 n=int(tokens[0])-1
 word=tokens[1]
-inv={}
-for i in range(len(word)):
-    inv[i]=word[i]
-inv = sorted(inv.items(), key=lambda x:x[1])
-for i in range(len(word)):
-    n=inv[n][0]
-    print(word[n],end='')
+l=len(word)
+inv=[0]*l
+for i in range(l):
+    inv[i]=(order.find(word[i])<<16) + i
 
+inv = sorted(inv)
+
+for i in range(len(word)):
+    n=inv[n]%(65536)
+    print(word[n],end='')
