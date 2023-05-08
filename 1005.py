47ee2ee5b0aee4e46327d26579e2dd1f1c756499
Mikhail Belyankin <undermind@users.noreply.github.com>
2023-05-08 11:47:51 +0300

    dynamic for 1005, TL3

--- 1005.py
+++ 1005.py
@@ -1,24 +1,53 @@
 import sys
-#tokens = sys.stdin.read().split()
-tokens = ['5','5','8','13','27','14']
+tokens = sys.stdin.read().split()
+#tokens = ['5','5','8','13','27','14']
 #tokens = ['5','5','5','4','3','3']
 #tokens = ['4','15','16','30','2']
+#tokens = ['1','1']
+#tokens = ['3','2','2','2']
 n=int(tokens[0])
 w=[0]*n
 
 sum=0
 
-sums=[[]*0]*1000000
+sums=[None]*1000000
 
 for i in range(n):
     w[i]=int(tokens[i+1])
     sum+=w[i]
-    sums[w[i]]=[set(i)]
+#w.sort()
+#for i in range(n):
+    if sums[w[i]]:
+      sums[w[i]]=sums[w[i]]|{1<<i}
+    else: sums[w[i]]={1<<i}
+
+if n==1: 
+   print(w[0])
+   exit()
+
+for i in range(sum+1):
+  if sums[i]:
+    for z in sums[i]:
+      for j in range(n):
+        if not ((1<<j) & z):
+           y=i+w[j] #achive new sum
+           if sums[y]: sums[y] =sums[y]|{z|(1<<j)}
+           else:sums[y]={z|(1<<j)}
 
 
+#for i in range(sum+1):if sums[i]:print(i,sums[i])
 
-print(sums)
-
+i=sum//2
+while True:
+  if sums[i] and sums[sum-i]: #есть сумма пополам
+      if len(sums[i])+len(sums[sum-i])>1:
+         for a in sums[i]:
+            for b in sums[sum-i]:
+               if (a & b ==0) and (a | b == (1<<n)-1):
+                  print(sum-i*2)
+                  exit()
+  i-=1
+#      print(i,len(sums[sum//2+i]))
 
 #for i in range(sum//2):
 #  for el in sums[i]:
